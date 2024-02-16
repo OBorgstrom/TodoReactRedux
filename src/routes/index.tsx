b43ecf815ae296dcbce9d24e35b5feee19a94ecd
Linus Åkesson <linusakesson3@gmail.com>
2024-02-16 09:49:09 +0100

    feat: added zustand och redux logo and landingpage design

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import reduxLogo from '../assets/redux-logo.png'
+import zustandLogo from '../assets/zustand-logo.png'
+
 function HomeComponent() {
   return (
-    <div className="p-2">
+    <div className="landing">
+      <div className="logos">
+        <img src={reduxLogo} alt="redux-logo" className="redux-logo" />
+        <span className="separator" />
+        <img src={zustandLogo} alt="redux-logo" className="zustand-logo" />
+      </div>
       <h3>Welcome Home!</h3>
     </div>
   )
