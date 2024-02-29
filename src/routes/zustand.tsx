e7912035e29dbb228eb5eb8a1ef152995fdd02de
Linus Åkesson <linusakesson3@gmail.com>
2024-02-29 22:28:05 +0100

    feat: added more information about zustand

--- src/routes/zustand.tsx
+++ src/routes/zustand.tsx
@@ -9,6 +9,7 @@ function ZustandComponent() {
       <h1>Zustand information</h1>
       <section className="informations-section">
         <article>
+          <h3>A little about zustand</h3>
           <p>
             Zustand is a small, fast and scaleable bearbones state-management
             solution for JavaScript applications. It&apos;s based on the concept
@@ -53,10 +54,44 @@ function ZustandComponent() {
       </section>
       <section className="informations-section">
         <ZustandCodePreview />
-        <p>
-          Here is an example of how you can use Zustand to manage state in a
-          React application:
-        </p>
+        <article>
+          <h3>How to use Zustand</h3>
+          <p>
+            To use Zustand in your application, you first need to create a
+            store. A store is an object that holds your application&apos;s state
+            and provides methods for updating that state. You can create a store
+            using the <code>create</code> function from the <code>zustand</code>{' '}
+            package.
+          </p>
+          <p>
+            Here&apos;s an example of how you might create a store to manage a
+            counter in a React application:
+          </p>
+        </article>
+      </section>
+      <section className="informations-section">
+        <article>
+          <h3>When to use zustand</h3>
+          <p>
+            Zustand is a good choice for small to medium-sized applications
+            where you want a simple, fast, and scaleable state management
+            solution. It&apos;s a good choice for applications that don&apos;t
+            need the complexity of larger state management libraries like Redux
+            or MobX.
+          </p>
+          <p>
+            Zustand is also a good choice for applications that need to be
+            performant. It has a minimal impact on performance and uses a
+            subscription model to update components only when necessary,
+            reducing unnecessary renders.
+          </p>
+          <p>
+            Finally, Zustand is a good choice for applications that need to be
+            flexible. It&apos;s not tied to a specific framework, so you can use
+            it in any JavaScript application. This makes it a versatile choice
+            for a variety of projects.
+          </p>
+        </article>
       </section>
     </div>
   )
